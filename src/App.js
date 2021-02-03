b9e0ce71ffa2e091e5c9cf06688b642f1455c14c
mayra paez <maypaez_ba_90@hotmail.com>
2021-02-03 20:40:37 -0300

    card contador

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/NavBarComponent/NavBarComponent';
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-
+import CardCounter from "./components/CardCounter/CardCounter";
 
 function App() {
   return (
   <div>
     <NavbarComponent />
     <ItemListContainer greeting={"Bienvenidos a Eneldo Mercado Natural"} />
+    <CardCounter product="Mantequilla de Mani" productDescription="mantequilla de mani sin azucar 100% natural" price="$280" />
   </div>
   );}
 
